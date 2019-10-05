0306ba7f205c6a41213020ea7c9421fed25ff885
miladjalalli <miladjalalli@yahoo.com>
2019-10-05 10:12:23 +0330

    first initialize

--- CommonUtils/src/main/java/me/miladjalali/commonutils/InputValidatorHelper.java
+++ CommonUtils/src/main/java/me/miladjalali/commonutils/InputValidatorHelper.java
@@ -7,15 +7,17 @@ public class InputValidatorHelper {
     int error;
 
     public InputValidatorHelper() {
-
+        error = 0;
     }
 
     public boolean ValidateInputName(EditText editText) {
         String text = GetTextOfEditText(editText);
         if (text.length() < 3) {
+            error++;
             editText.setError("حداقل 3 کاراکتر وارد کنید");
             return false;
         } else if (text.length() > 40) {
+            error++;
             editText.setError("حداکثر 40 کاراکتر مجاز است");
             return false;
         } else {
@@ -35,5 +37,9 @@ public class InputValidatorHelper {
 
     public String GetTextOfEditText(EditText editText) {
         return editText.getText().toString().trim();
+    }
+
+    public boolean InputsHasError(){
+        return error>0;
     }
 }
