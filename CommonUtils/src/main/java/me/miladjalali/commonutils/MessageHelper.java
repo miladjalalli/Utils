44671c10601669c5c611e4ed263b073efb273a25
miladjalalli <miladjalalli@yahoo.com>
2019-10-17 09:59:10 +0330

    Asset Helper

--- CommonUtils/src/main/java/me/miladjalali/commonutils/MessageHelper.java
+++ CommonUtils/src/main/java/me/miladjalali/commonutils/MessageHelper.java
@@ -3,13 +3,18 @@ package me.miladjalali.commonutils;
 import android.content.Context;
 import android.content.Intent;
 import android.provider.Settings;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
+
+import static android.content.Context.LAYOUT_INFLATER_SERVICE;
 
 public class MessageHelper {
 
@@ -80,6 +85,34 @@ public class MessageHelper {
         Snackbar snackbar = Snackbar.make(view, context.getString(R.string.PleaseBackButtonAgainForExit), Snackbar.LENGTH_INDEFINITE).setActionTextColor(context.getResources().getColor(android.R.color.white));
         snackbar.getView().setBackgroundColor(context.getResources().getColor(android.R.color.black));
         snackbar.setDuration(2000);
+        snackbar.show();
+    }
+
+    public void TwoChooseSnackbar(Context context, View view, String text, View.OnClickListener onClickListenerPositive, View.OnClickListener onClickListenerNegative)
+    {
+        // Create the Snackbar
+        LinearLayout.LayoutParams objLayoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        Snackbar snackbar = Snackbar.make(view, "", Snackbar.LENGTH_LONG);
+        // Get the Snackbar's layout view
+        Snackbar.SnackbarLayout layout = (Snackbar.SnackbarLayout) snackbar.getView();
+        layout.setPadding(0,0,0,0);
+        // Hide the text
+        TextView textView = layout.findViewById(com.google.android.material.R.id.snackbar_text);
+        textView.setVisibility(View.INVISIBLE);
+
+        LayoutInflater mInflater = (LayoutInflater)context.getSystemService(LAYOUT_INFLATER_SERVICE);
+        // Inflate our custom view
+        View snackView = mInflater.inflate(R.layout.two_choice_snackbar, null);
+        // Configure the view
+        TextView txtPositive = snackView.findViewById(R.id.txtPositive);
+        txtPositive.setOnClickListener(onClickListenerPositive);
+
+        TextView txtNegative = snackView.findViewById(R.id.txtNegative);
+        txtNegative.setOnClickListener(onClickListenerNegative);
+
+        // Add the view to the Snackbar's layout
+        layout.addView(snackView, objLayoutParams);
+        // Show the Snackbar
         snackbar.show();
     }
 
