2dbe6c5e69a820fed419308857db7e75a6d5558f
miladjalalli <miladjalalli@yahoo.com>
2019-10-21 09:45:27 +0330

    Asset Helper

--- CommonUtils/src/main/java/me/miladjalali/commonutils/UriHelper.java
+++ CommonUtils/src/main/java/me/miladjalali/commonutils/UriHelper.java
@@ -9,18 +9,53 @@ import java.io.InputStream;
 
 public class UriHelper {
 
-    public static byte[] getBytes(Context context,Uri uri) throws IOException {
 
-        InputStream inputStream =   context.getContentResolver().openInputStream(uri);
+    /**
+     * get bytes array from Uri.
+     *
+     * @param context current context.
+     * @param uri uri fo the file to read.
+     * @return a bytes array.
+     * @throws IOException
+     */
+    public static byte[] getBytes(Context context, Uri uri) throws IOException {
+        InputStream iStream = context.getContentResolver().openInputStream(uri);
+        try {
+            return getBytes(iStream);
+        } finally {
+            // close the stream
+            try {
+                iStream.close();
+            } catch (IOException ignored) { /* do nothing */ }
+        }
+    }
 
+
+    /**
+     * get bytes from input stream.
+     *
+     * @param inputStream inputStream.
+     * @return byte array read from the inputStream.
+     * @throws IOException
+     */
+    public static byte[] getBytes(InputStream inputStream) throws IOException {
+
+        byte[] bytesResult = null;
         ByteArrayOutputStream byteBuffer = new ByteArrayOutputStream();
         int bufferSize = 1024;
         byte[] buffer = new byte[bufferSize];
-
-        int len = 0;
-        while ((len = inputStream.read(buffer)) != -1) {
-            byteBuffer.write(buffer, 0, len);
+        try {
+            int len;
+            while ((len = inputStream.read(buffer)) != -1) {
+                byteBuffer.write(buffer, 0, len);
+            }
+            bytesResult = byteBuffer.toByteArray();
+        } finally {
+            // close the stream
+            try{ byteBuffer.close(); } catch (IOException ignored){ /* do nothing */ }
         }
-        return byteBuffer.toByteArray();
+        return bytesResult;
     }
+
+
 }
