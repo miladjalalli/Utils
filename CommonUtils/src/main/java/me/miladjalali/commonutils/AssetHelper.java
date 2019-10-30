0c784d970732711e3adbfc8de9a0b7198049339e
miladjalalli <miladjalalli@yahoo.com>
2019-10-30 14:15:21 +0330

    Asset Helper

--- CommonUtils/src/main/java/me/miladjalali/commonutils/AssetHelper.java
+++ CommonUtils/src/main/java/me/miladjalali/commonutils/AssetHelper.java
@@ -16,8 +16,7 @@ public class AssetHelper {
             byte[] buffer = new byte[size];
             is.read(buffer);
             is.close();
-            json = new String(buffer, "utf-8");
-
+            json = new String(buffer, StandardCharsets.UTF_8);
         } catch (IOException ex) {
             ex.printStackTrace();
             return null;
