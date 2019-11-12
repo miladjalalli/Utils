fbaa80e642446271312e0acd832f3466f8950791
miladjalalli <miladjalalli@yahoo.com>
2019-11-12 15:28:57 +0330

    Asset Helper

--- CommonUtils/src/main/java/me/miladjalali/commonutils/UriHelper.java
+++ CommonUtils/src/main/java/me/miladjalali/commonutils/UriHelper.java
@@ -1,7 +1,9 @@
 package me.miladjalali.commonutils;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.net.Uri;
+import android.provider.OpenableColumns;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -57,5 +59,26 @@ public class UriHelper {
         return bytesResult;
     }
 
+    public String getFileNameWithExtention(Context context,Uri uri) {
+        String result = null;
+        if (uri.getScheme().equals("content")) {
+            Cursor cursor = context.getContentResolver().query(uri, null, null, null, null);
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                }
+            } finally {
+                cursor.close();
+            }
+        }
+        if (result == null) {
+            result = uri.getPath();
+            int cut = result.lastIndexOf('/');
+            if (cut != -1) {
+                result = result.substring(cut + 1);
+            }
+        }
+        return result;
+    }
 
 }
